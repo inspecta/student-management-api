900d09093551bad9ed8087318b66b20c01610024
Derrick Mulinde <djmulinde@gmail.com>
2023-07-11 20:49:33 +0300

    Add response entity response types

--- src/main/java/com/example/students/students/StudentController.java
+++ src/main/java/com/example/students/students/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.students.students;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,13 +27,13 @@ public class StudentController {
     }
 
     @PostMapping
-    public void addNewStudent(@RequestBody Student student){
-        studentService.registerStudent(student);
+    public ResponseEntity<String> addNewStudent(@RequestBody Student student){
+        return studentService.registerStudent(student);
     }
 
     @DeleteMapping(path = "{studentId}")
-    public void deleteStudent(@PathVariable("studentId") Long studentId) {
-        studentService.removeStudent(studentId);
+    public ResponseEntity<String> deleteStudent(@PathVariable("studentId") Long studentId) {
+        return studentService.removeStudent(studentId);
     }
 
     @PutMapping(path = "{studentId}")
